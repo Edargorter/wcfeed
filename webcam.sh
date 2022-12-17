05bf6b95223466b8bb7d65c623471b01a24e2d8b
Zachary Bowditch <zdbowditch777@gmail.com>
2022-12-17 09:46:41 +0200

    CPU temp

--- webcam.sh
+++ webcam.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-directory=$(pwd)
+directory=$1
 filename="capture"
 ext="jpg"
 
-img=0
-mod=5
+count=0
+mod=$((5*12*30))
+
+sudo python -m http.server 80 --directory $directory > ${directory}/server.log 2>&1 &
 
 while :
 do
 	fswebcam -r 640-480 ${directory}/${filename}.$ext
+	vcgencmd measure_temp | cut -d "=" -f 2 | rev | cut -c 3- | rev > ${directory}/temp.txt
 	sleep 5
-	#img=$((($img + 1) % $mod))
+	count=$((($count + 1) % $mod))
+	if [ "$count" -eq "0" ]; then
+		tail -n 100 ${directory}/server.log > ${directory}/server.log;
+	fi
 done
